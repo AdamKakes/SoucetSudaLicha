08cb2b9e2942651f35cf2fcf468686d8f3b07c20
AdamKakes <adam.kakes.020@pslib.cz>
2022-03-21 14:09:15 +0100

    Přidejte soubory projektu.

---
+++ ukol2103/ukol2103.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+int suda(int x)
+{
+	int y = 0;
+	for (int i = 0; i <= x; i += 2)
+	{
+		y += i;
+	}
+	return y;
+}
+
+int licha(int x)
+{
+	int y = 0;
+	for (int i = 1; i <= x; i += 2)
+	{
+		y += i;
+	}
+	return y;
+}
+
+int main()
+{
+	printf("soucet sudych cisel: %d\n", suda(10));
+	printf("soucet lichych cisel:%d\n", licha(10));
+}
